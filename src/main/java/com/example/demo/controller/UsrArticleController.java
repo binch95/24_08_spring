c697693fe5618102a6a3dfd35227f637f9c3ee71
binch95 <qlscm95@gmail.com>
2024-08-09 15:19:04 +0900

    service구조 추가, 기능이전

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,77 +1,67 @@
 package com.example.demo.controller;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.service.ArticleService;
 import com.example.demo.vo.Article;
-
-import lombok.Data;
 @Controller
-@Data
 public class UsrArticleController {
-
-
-	int idcount;
-	List<Article> articles;
-	
-	public UsrArticleController() {
-		idcount = 0;
-		articles = new ArrayList<>();
-		
-		for(int i = 1; i <=10; i++) {
-			Article article = new Article(i,"test" + i,"test"+i);
-			articles.add(article);
-		}
-	}
-
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doadd(String title, String body){
-		int id = articles.size() +1;
-		Article article = new Article(id,title,body);
-		articles.add(article);
-		idcount++;
-		return article;
-	}
-	
-	@RequestMapping("/usr/article/doDelete")
-	@ResponseBody
-	public String dodelete(int id){
-		for (Article article:articles){
-			if(article.getId() == id) {
-				articles.remove(article);
-				return id + "번 글이 삭제되었습니다.";
-			}
-		}
-		return id + "번 글없음";
-	}
-	
-	@RequestMapping("/usr/article/doModify")
-	@ResponseBody
-	public Object domodify(int id, String title, String body){
-		
-		for (Article article:articles){
-			if(article.getId() == id) {
-				article.setTitle(title);
-				article.setBody(body);
-				return id + "번 글이 수정되었습니다." + getArticle();
-			}
-		}
-
-		return id + "번 글없음";
-	}
-	
+	@Autowired
+	private ArticleService articleService;
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public List<Article> getArticle() {
-		return articles;
-	}
-	
+	public Object getArticle(int id) {
 
+		Article article = articleService.getArticleById(id);
+
+		if (article == null) {
+			return id + "번 글은 없음";
+		}
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+
+		Article article = articleService.getArticleById(id);
+
+		if (article == null) {
+			return id + "번 글은 없음";
+		}
+
+		articleService.modifyArticle(id, title, body);
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = articleService.getArticleById(id);
+		if (article == null) {
+			return id + "번 글은 없음";
+		}
+		articleService.deleteArticle(id);
+		return id + "번 글이 삭제됨";
+	}
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = articleService.writeArticle(title, body);
+		return article;
+	}
+	@RequestMapping("/usr/article/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		return articleService.articles;
+	}
 }
--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -1,17 +1,16 @@
 package com.example.demo.vo;
 
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Article {
 		int id;
 		String title;
 		String body;
-	
-	public Article(int id, String title, String body) {
-		this.id=id;
-		this.title = title;
-		this.body = body;
-	}
-	
+		
 }
